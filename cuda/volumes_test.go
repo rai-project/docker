ecb22fc2592a3c6b075eb0cbda90580921d9aca0
Abdul Dakkak <abduld@wolfram.com>
2017-03-09 23:30:25 +0000

    add test case

--- cuda/volumes_test.go
+++ cuda/volumes_test.go
@@ -1,1 +1,16 @@
 package cuda
+
+import (
+	"testing"
+
+	nvidiasmi "github.com/rai-project/nvidia-smi"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetVolume(t *testing.T) {
+	version := nvidiasmi.Info.DriverVersion
+	volume, ver, err := getVolume("nvidia_driver_" + version)
+	assert.NoError(t, err)
+	assert.Equal(t, version, ver)
+	assert.NotNil(t, volume)
+}
