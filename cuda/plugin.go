3b52d60ea81309dfa96a7108bd5d2f75b5d6ffce
Abdul Dakkak <abduld@wolfram.com>
2017-03-09 20:34:06 +0000

    start working on cuda docker plugin

---
+++ cuda/plugin.go
@@ -0,0 +1,101 @@
+package cuda
+
+import (
+	"fmt"
+	"os/user"
+	"path"
+	"regexp"
+	"strconv"
+
+	"github.com/docker/go-plugins-helpers/volume"
+	"github.com/pkg/errors"
+)
+
+type CUDADriver struct{}
+
+func getVolume(name string) (*Volume, string, error) {
+	re := regexp.MustCompile("^([a-zA-Z0-9_.-]+)_([0-9.]+)$")
+	m := re.FindStringSubmatch(name)
+	if len(m) != 3 {
+		return nil, "", errors.Errorf("%v is not a valid volume format", name)
+	}
+	volume, version := VolumeMap[m[1]], m[2]
+	if volume == nil {
+		return nil, "", errors.Errorf("%v volume is not supported", m[1])
+	}
+	return volume, version, nil
+}
+
+func (CUDADriver) Create(req volume.Request) volume.Response {
+	vol, version, err := getVolume(req.Name)
+	if err != nil {
+		return volume.Response{
+			Err: errors.Wrapf(err, "failed to create %v volume", req.Name).Error(),
+		}
+	}
+	// The volume version requested needs to match the volume version in cache
+	if version != vol.Version {
+		return volume.Response{
+			Err: errors.Errorf("volume version mismatch %v != %v", version, vol.Version).Error(),
+		}
+	}
+	ok, err := vol.Exists()
+	if !ok {
+		vol.Create(LinkStrategy{})
+	}
+	return volume.Response{}
+}
+
+func (CUDADriver) List(req volume.Request) volume.Response {
+	var r volume.Response
+	for _, vol := range VolumeMap {
+		versions, err := vol.ListVersions()
+		if err != nil {
+			log.WithError(err).Error("failed to get volume %v version information", vol.Name)
+			continue
+		}
+		for _, v := range versions {
+			r.Volumes = append(r.Volumes, &volume.Volume{
+				Name:       fmt.Sprintf("%s_%s", vol.Name, v),
+				Mountpoint: path.Join(vol.Path, v),
+			})
+		}
+	}
+	return r
+}
+
+func (CUDADriver) Get(req volume.Request) volume.Response {
+	return volume.Response{}
+}
+
+func (CUDADriver) Remove(req volume.Request) volume.Response {
+	return volume.Response{}
+}
+
+func (CUDADriver) Path(req volume.Request) volume.Response {
+	return volume.Response{}
+}
+
+func (CUDADriver) Mount(req volume.MountRequest) volume.Response {
+	return volume.Response{}
+}
+
+func (CUDADriver) Unmount(req volume.UnmountRequest) volume.Response {
+	return volume.Response{}
+}
+
+func (CUDADriver) Capabilities(req volume.Request) volume.Response {
+	return volume.Response{
+		Capabilities: volume.Capability{
+			Scope: "local",
+		},
+	}
+}
+
+func Serve() {
+	d := CUDADriver{}
+	h := volume.NewHandler(d)
+	u, _ := user.Lookup("ubuntu")
+	gid, _ := strconv.Atoi(u.Gid)
+	h.ServeUnix("cuda_plugin", gid)
+}
--- cuda/volumes.go
+++ cuda/volumes.go
@@ -3,14 +3,21 @@ package cuda
 import (
 	"bufio"
 	"bytes"
+	"debug/elf"
+	"encoding/binary"
+	"fmt"
 	"io"
+	"io/ioutil"
+	"os"
 	"os/exec"
 	"path"
 	"path/filepath"
+	"regexp"
 	"strings"
 
-	"github.com/rai-project/cudainfo"
+	"github.com/rai-project/config"
 	"github.com/rai-project/ldcache"
+	nvidiasmi "github.com/rai-project/nvidia-smi"
 )
 
 const (
@@ -41,77 +48,196 @@ type Volume struct {
 	dirs    []volumeDir
 }
 
-type VolumeMap map[string]*Volume
+type volumeMap map[string]*Volume
 
-var Volumes = []VolumeInfo{
-	{
-		Name:         "nvidia_driver",
-		Mountpoint:   "/usr/local/nvidia",
-		MountOptions: "ro",
-		Components: components{
-			"binaries": {
-				//"nvidia-modprobe",       // Kernel module loader
-				//"nvidia-settings",       // X server settings
-				//"nvidia-xconfig",        // X xorg.conf editor
-				"nvidia-cuda-mps-control", // Multi process service CLI
-				"nvidia-cuda-mps-server",  // Multi process service server
-				"nvidia-debugdump",        // GPU coredump utility
-				"nvidia-persistenced",     // Persistence mode utility
-				"nvidia-smi",              // System management interface
-			},
-			"libraries": {
-				// ------- X11 -------
+var (
+	VolumeMap   volumeMap
+	VolumesPath = "/var/lib/nvidia-docker/volumes"
+	Volumes     = []VolumeInfo{
+		{
+			Name:         "nvidia_driver",
+			Mountpoint:   "/usr/local/nvidia",
+			MountOptions: "ro",
+			Components: components{
+				"binaries": {
+					//"nvidia-modprobe",       // Kernel module loader
+					//"nvidia-settings",       // X server settings
+					//"nvidia-xconfig",        // X xorg.conf editor
+					"nvidia-cuda-mps-control", // Multi process service CLI
+					"nvidia-cuda-mps-server",  // Multi process service server
+					"nvidia-debugdump",        // GPU coredump utility
+					"nvidia-persistenced",     // Persistence mode utility
+					"nvidia-smi",              // System management interface
+				},
+				"libraries": {
+					// ------- X11 -------
 
-				//"libnvidia-cfg.so",  // GPU configuration (used by nvidia-xconfig)
-				//"libnvidia-gtk2.so", // GTK2 (used by nvidia-settings)
-				//"libnvidia-gtk3.so", // GTK3 (used by nvidia-settings)
-				//"libnvidia-wfb.so",  // Wrapped software rendering module for X server
-				//"libglx.so",         // GLX extension module for X server
+					//"libnvidia-cfg.so",  // GPU configuration (used by nvidia-xconfig)
+					//"libnvidia-gtk2.so", // GTK2 (used by nvidia-settings)
+					//"libnvidia-gtk3.so", // GTK3 (used by nvidia-settings)
+					//"libnvidia-wfb.so",  // Wrapped software rendering module for X server
+					//"libglx.so",         // GLX extension module for X server
 
-				// ----- Compute -----
+					// ----- Compute -----
 
-				"libnvidia-ml.so",              // Management library
-				"libcuda.so",                   // CUDA driver library
-				"libnvidia-ptxjitcompiler.so",  // PTX-SASS JIT compiler (used by libcuda)
-				"libnvidia-fatbinaryloader.so", // fatbin loader (used by libcuda)
-				"libnvidia-opencl.so",          // NVIDIA OpenCL ICD
-				"libnvidia-compiler.so",        // NVVM-PTX compiler for OpenCL (used by libnvidia-opencl)
-				//"libOpenCL.so",               // OpenCL ICD loader
+					"libnvidia-ml.so",              // Management library
+					"libcuda.so",                   // CUDA driver library
+					"libnvidia-ptxjitcompiler.so",  // PTX-SASS JIT compiler (used by libcuda)
+					"libnvidia-fatbinaryloader.so", // fatbin loader (used by libcuda)
+					"libnvidia-opencl.so",          // NVIDIA OpenCL ICD
+					"libnvidia-compiler.so",        // NVVM-PTX compiler for OpenCL (used by libnvidia-opencl)
+					//"libOpenCL.so",               // OpenCL ICD loader
 
-				// ------ Video ------
+					// ------ Video ------
 
-				"libvdpau_nvidia.so",  // NVIDIA VDPAU ICD
-				"libnvidia-encode.so", // Video encoder
-				"libnvcuvid.so",       // Video decoder
-				"libnvidia-fbc.so",    // Framebuffer capture
-				"libnvidia-ifr.so",    // OpenGL framebuffer capture
+					"libvdpau_nvidia.so",  // NVIDIA VDPAU ICD
+					"libnvidia-encode.so", // Video encoder
+					"libnvcuvid.so",       // Video decoder
+					"libnvidia-fbc.so",    // Framebuffer capture
+					"libnvidia-ifr.so",    // OpenGL framebuffer capture
 
-				// ----- Graphic -----
+					// ----- Graphic -----
 
-				// XXX In an ideal world we would only mount nvidia_* vendor specific libraries and
-				// install ICD loaders inside the container. However, for backward compatibility reason
-				// we need to mount everything. This will hopefully change once GLVND is well established.
+					// XXX In an ideal world we would only mount nvidia_* vendor specific libraries and
+					// install ICD loaders inside the container. However, for backward compatibility reason
+					// we need to mount everything. This will hopefully change once GLVND is well established.
 
-				"libGL.so",         // OpenGL/GLX legacy _or_ compatibility wrapper (GLVND)
-				"libGLX.so",        // GLX ICD loader (GLVND)
-				"libOpenGL.so",     // OpenGL ICD loader (GLVND)
-				"libGLESv1_CM.so",  // OpenGL ES v1 common profile legacy _or_ ICD loader (GLVND)
-				"libGLESv2.so",     // OpenGL ES v2 legacy _or_ ICD loader (GLVND)
-				"libEGL.so",        // EGL ICD loader
-				"libGLdispatch.so", // OpenGL dispatch (GLVND) (used by libOpenGL, libEGL and libGLES*)
+					"libGL.so",         // OpenGL/GLX legacy _or_ compatibility wrapper (GLVND)
+					"libGLX.so",        // GLX ICD loader (GLVND)
+					"libOpenGL.so",     // OpenGL ICD loader (GLVND)
+					"libGLESv1_CM.so",  // OpenGL ES v1 common profile legacy _or_ ICD loader (GLVND)
+					"libGLESv2.so",     // OpenGL ES v2 legacy _or_ ICD loader (GLVND)
+					"libEGL.so",        // EGL ICD loader
+					"libGLdispatch.so", // OpenGL dispatch (GLVND) (used by libOpenGL, libEGL and libGLES*)
 
-				"libGLX_nvidia.so",         // OpenGL/GLX ICD (GLVND)
-				"libEGL_nvidia.so",         // EGL ICD (GLVND)
-				"libGLESv2_nvidia.so",      // OpenGL ES v2 ICD (GLVND)
-				"libGLESv1_CM_nvidia.so",   // OpenGL ES v1 common profile ICD (GLVND)
-				"libnvidia-eglcore.so",     // EGL core (used by libGLES* or libGLES*_nvidia and libEGL_nvidia)
-				"libnvidia-egl-wayland.so", // EGL wayland extensions (used by libEGL_nvidia)
-				"libnvidia-glcore.so",      // OpenGL core (used by libGL or libGLX_nvidia)
-				"libnvidia-tls.so",         // Thread local storage (used by libGL or libGLX_nvidia)
-				"libnvidia-glsi.so",        // OpenGL system interaction (used by libEGL_nvidia)
+					"libGLX_nvidia.so",         // OpenGL/GLX ICD (GLVND)
+					"libEGL_nvidia.so",         // EGL ICD (GLVND)
+					"libGLESv2_nvidia.so",      // OpenGL ES v2 ICD (GLVND)
+					"libGLESv1_CM_nvidia.so",   // OpenGL ES v1 common profile ICD (GLVND)
+					"libnvidia-eglcore.so",     // EGL core (used by libGLES* or libGLES*_nvidia and libEGL_nvidia)
+					"libnvidia-egl-wayland.so", // EGL wayland extensions (used by libEGL_nvidia)
+					"libnvidia-glcore.so",      // OpenGL core (used by libGL or libGLX_nvidia)
+					"libnvidia-tls.so",         // Thread local storage (used by libGL or libGLX_nvidia)
+					"libnvidia-glsi.so",        // OpenGL system interaction (used by libEGL_nvidia)
+				},
 			},
 		},
-	},
+	}
+)
+
+type FileCloneStrategy interface {
+	Clone(src, dst string) error
+}
+
+type LinkStrategy struct{}
+
+func (s LinkStrategy) Clone(src, dst string) error {
+	return os.Link(src, dst)
+}
+
+func (v *Volume) Create(s FileCloneStrategy) (err error) {
+	root := path.Join(v.Path, v.Version)
+	if err = os.MkdirAll(root, 0755); err != nil {
+		return
+	}
+	defer func() {
+		if err != nil {
+			v.Remove()
+		}
+	}()
+
+	for _, d := range v.dirs {
+		vpath := path.Join(root, d.name)
+		if err := os.MkdirAll(vpath, 0755); err != nil {
+			return err
+		}
+
+		// For each file matching the volume components (blacklist excluded), create a hardlink/copy
+		// of it inside the volume directory. We also need to create soname symlinks similar to what
+		// ldconfig does since our volume will only show up at runtime.
+		for _, f := range d.files {
+			obj, err := elf.Open(f)
+			if err != nil {
+				return fmt.Errorf("%s: %v", f, err)
+			}
+			defer obj.Close()
+
+			ok, err := blacklisted(f, obj)
+			if err != nil {
+				return fmt.Errorf("%s: %v", f, err)
+			}
+			if ok {
+				continue
+			}
+
+			l := path.Join(vpath, path.Base(f))
+			if err := s.Clone(f, l); err != nil {
+				return err
+			}
+			soname, err := obj.DynString(elf.DT_SONAME)
+			if err != nil {
+				return fmt.Errorf("%s: %v", f, err)
+			}
+			if len(soname) > 0 {
+				l = path.Join(vpath, soname[0])
+				if err := os.Symlink(path.Base(f), l); err != nil && !os.IsExist(err) {
+					return err
+				}
+				// XXX Many applications (wrongly) assume that libcuda.so exists (e.g. with dlopen)
+				// Hardcode the libcuda symlink for the time being.
+				if strings.HasPrefix(soname[0], "libcuda") {
+					l = strings.TrimRight(l, ".0123456789")
+					if err := os.Symlink(path.Base(f), l); err != nil && !os.IsExist(err) {
+						return err
+					}
+				}
+				// XXX GLVND requires this symlink for indirect GLX support
+				// It won't be needed once we have an indirect GLX vendor neutral library.
+				if strings.HasPrefix(soname[0], "libGLX_nvidia") {
+					l = strings.Replace(l, "GLX_nvidia", "GLX_indirect", 1)
+					if err := os.Symlink(path.Base(f), l); err != nil && !os.IsExist(err) {
+						return err
+					}
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func (v *Volume) Remove(version ...string) error {
+	vv := v.Version
+	if len(version) == 1 {
+		vv = version[0]
+	}
+	return os.RemoveAll(path.Join(v.Path, vv))
+}
+
+func (v *Volume) ListVersions() ([]string, error) {
+	dirs, err := ioutil.ReadDir(v.Path)
+	if os.IsNotExist(err) {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+	versions := make([]string, len(dirs))
+	for i := range dirs {
+		versions[i] = dirs[i].Name()
+	}
+	return versions, nil
+}
+
+func (v *Volume) Exists(version ...string) (bool, error) {
+	vv := v.Version
+	if len(version) == 1 {
+		vv = version[0]
+	}
+	_, err := os.Stat(path.Join(v.Path, vv))
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return true, err
 }
 
 func which(bins ...string) ([]string, error) {
@@ -139,11 +265,53 @@ func which(bins ...string) ([]string, error) {
 	return paths, nil
 }
 
-func LookupVolumes(prefix string) (vols VolumeMap, err error) {
-	drv, err := cudainfo.GetDriverVersion()
+func blacklisted(file string, obj *elf.File) (bool, error) {
+	lib := regexp.MustCompile(`^.*/lib([\w-]+)\.so[\d.]*$`)
+	glcore := regexp.MustCompile(`libnvidia-e?glcore\.so`)
+	gldispatch := regexp.MustCompile(`libGLdispatch\.so`)
+
+	if m := lib.FindStringSubmatch(file); m != nil {
+		switch m[1] {
+
+		// Blacklist EGL/OpenGL libraries issued by other vendors
+		case "EGL":
+			fallthrough
+		case "GLESv1_CM":
+			fallthrough
+		case "GLESv2":
+			fallthrough
+		case "GL":
+			deps, err := obj.DynString(elf.DT_NEEDED)
+			if err != nil {
+				return false, err
+			}
+			for _, d := range deps {
+				if glcore.MatchString(d) || gldispatch.MatchString(d) {
+					return false, nil
+				}
+			}
+			return true, nil
+
+		// Blacklist TLS libraries using the old ABI (!= 2.3.99)
+		case "nvidia-tls":
+			const abi = 0x6300000003
+			s, err := obj.Section(".note.ABI-tag").Data()
+			if err != nil {
+				return false, err
+			}
+			return binary.LittleEndian.Uint64(s[24:]) != abi, nil
+		}
+	}
+	return false, nil
+}
+
+func LookupVolumes(prefix string) (vols volumeMap, err error) {
+	info, err := nvidiasmi.New()
 	if err != nil {
 		return nil, err
 	}
+
+	drv := info.DriverVersion
 
 	cache, err := ldcache.Open()
 	if err != nil {
@@ -155,7 +323,7 @@ func LookupVolumes(prefix string) (vols VolumeMap, err error) {
 		}
 	}()
 
-	vols = make(VolumeMap, len(Volumes))
+	vols = make(volumeMap, len(Volumes))
 
 	for i := range Volumes {
 		vol := &Volume{
@@ -183,4 +351,10 @@ func LookupVolumes(prefix string) (vols VolumeMap, err error) {
 		vols[vol.Name] = vol
 	}
 	return
+}
+
+func init() {
+	config.AfterInit(func() {
+		VolumeMap, _ = LookupVolumes(VolumesPath)
+	})
 }
